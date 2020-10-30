69d03c5b519d1052c4dd5e7cbe8943de2afe9563
Nastya-Shchukina <egorshchukin2602@mail.ru>
2020-10-30 12:09:36 +0300

    Выполнена домашняя работа №1

---
+++ homework1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double side;
+            double angleInDegrees;
+            Console.WriteLine("Введите сторону:");
+            side = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите угол: ");
+            angleInDegrees = double.Parse(Console.ReadLine());
+            double angleInRadians = angleInDegrees * Math.PI / 180;
+            Console.WriteLine(("Площадь ромба: " + (side * side * Math.Round(Math.Sin(angleInRadians), 2))));
+            Console.WriteLine(("Периметр ромба: " + (side + side + side + side)));
+
+            Console.ReadKey();
