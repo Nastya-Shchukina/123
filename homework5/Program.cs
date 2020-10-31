a1d1da9b42cfcd7f72ec2223f41dd58041923b1c
Nastya-Shchukina <egorshchukin2602@mail.ru>
2020-10-31 03:03:25 +0300

    Выполнено домашнее задание 5

---
+++ homework5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string lineRus = Console.ReadLine();
+
+            string lineEng = Transliteration(lineRus);
+            Console.WriteLine(lineEng);
+
+            Console.ReadKey();
+        }
+
+        static string Transliteration(string str)
+        {
+            str = str.Replace("Ф", "A");
+            str = str.Replace("Б", "B");
+            str = str.Replace("В", "V");
+            str = str.Replace("Г", "G");
+            str = str.Replace("Д", "D");
+            str = str.Replace("Е", "E");
+            str = str.Replace("Ё", "E");
+            str = str.Replace("Ж", "ZH");
+            str = str.Replace("З", "Z");
+            str = str.Replace("И", "I");
+            str = str.Replace("Й", "I");
+            str = str.Replace("К", "K");
+            str = str.Replace("Л", "L");
+            str = str.Replace("М", "M");
+            str = str.Replace("Н", "N");
+            str = str.Replace("О", "O");
+            str = str.Replace("П", "P");
+            str = str.Replace("Р", "R");
+            str = str.Replace("С", "S");
+            str = str.Replace("Т", "T");
+            str = str.Replace("У", "U");
+            str = str.Replace("Ф", "F");
+            str = str.Replace("Х", "KH");
+            str = str.Replace("Ц", "TS");
+            str = str.Replace("Ч", "CH");
+            str = str.Replace("Ш", "SH");
+            str = str.Replace("Щ", "SHCH");
+            str = str.Replace("Ъ", "IE");
+            str = str.Replace("Ы", "Y");
+            str = str.Replace("Ь", "");
+            str = str.Replace("Э", "E");
+            str = str.Replace("Ю", "IU");
+            str = str.Replace("Я", "IA");
+
+            return str;
+        }
+        
+    }
+}
