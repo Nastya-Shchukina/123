aaa92b30658241001283088b7c1590597aa11ba8
Nastya-Shchukina <egorshchukin2602@mail.ru>
2020-11-05 23:06:13 +0300

    исправлена ошибка

---
+++ testim4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testim4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательно 9 чисел");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int e = int.Parse(Console.ReadLine());
+            int f = int.Parse(Console.ReadLine());
+            int g = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            int i = int.Parse(Console.ReadLine());
+
+
+
+            double answer = mathf(a, b, c) + mathf(d, e, f) + mathf(g, h, i);
+            Console.WriteLine(answer);
+        }
+        public static double mathf(double x, double y, double z)
+        {
+            return (Math.Sqrt((x + Math.Tan(y) * Math.Tan(y)) / z));
+        }
+    }
+}
+
